d780992edd18d78af92f0df00e96a8e837f74989
Amiiiiiiiin <realaminem@gmail.com>
2022-05-04 10:36:08 +0200

    Add project files.

---
+++ SYSProjectReciever/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdpReciever
+{
+    class Program
+    {
+        private const int Port = 7000; //TODO husk port
+        static void Main()
+        {
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Any, Port);
+                using (UdpClient socket = new UdpClient(ipEndPoint))
+                {
+                    IPEndPoint remoteEndPoint = new IPEndPoint(0, 0);
+                    while (true)
+                    {
+                        Console.WriteLine("Waiting for broadcast {0}", socket.Client.LocalEndPoint);
+                        byte[] datagramReceived = socket.Receive(ref remoteEndPoint);
+
+                        string message = Encoding.ASCII.GetString(datagramReceived, 0, datagramReceived.Length);
+                        Console.WriteLine("Receives {0} bytes from {1} port {2} message {3}", datagramReceived.Length,
+                            remoteEndPoint.Address, remoteEndPoint.Port, message);
+                        // TODO Kalde rest post 
+                        
+                    }
+                }
+        }
+
+        private static void Parse(string response)
+        {
+            string[] parts = response.Split(' ');
+            foreach (string part in parts)
+            {
+                Console.WriteLine(part);
+            }
+            string lightLine = parts[6];
+            string lightStr = lightLine.Substring(lightLine.IndexOf(": ") + 2);
+            Console.WriteLine(lightStr);
+        }
+    }
+}
